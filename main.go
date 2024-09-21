d871afb350504f868716363ea4c30c7be09ac6a7
Jesus D'mitri Medina Novelo <00396910@anahuac.mx>
2024-09-20 20:03:11 -0600

    basico

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"analytics/dbgen"
+	"context"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"os"
+)
 
 func main() {
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Cant connect to the db")
+	}
 	r := gin.Default()
 	r.GET("/authors", func(c *gin.Context) {
+		authors, err := dbgen.New(pool).ListAuthors(context.Background())
+		if err != nil {
+			log.Fatal("DB error", err)
+		}
 		c.JSON(200, gin.H{
-			"author": GetAut,
+			"author": authors,
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run("localhost:8000") // listen and serve on 0.0.0.0:8080
 }
