193629de6d7643413ccc55dde36023c9b5416cd4
victor <vjbsh@hotmail.com>
2024-09-20 21:45:17 -0600

    location handler

--- locations.go
+++ locations.go
@@ -1,7 +1,30 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type locationInput struct {
+	Lat  float64 `json:"lat"`
+	Long float64 `json:"long"`
+}
 
 func (h Handler) LocationHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("foo"))
+	var input locationInput
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	response, err := json.Marshal(input)
+	if err != nil {
+		http.Error(w, "Error processing input", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
 }
