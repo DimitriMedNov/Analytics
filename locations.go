11ad84b67da18f1fc27fda755249851919b5ae5a
victor <vjbsh@hotmail.com>
2024-09-20 21:32:10 -0600

    handler

--- locations.go
+++ locations.go
@@ -1,1 +1,7 @@
 package main
+
+import "net/http"
+
+func (h Handler) LocationHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("foo"))
+}
--- main.go
+++ main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"context"
-	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Handler struct{ pool *pgxpool.Pool }
+
 func main() {
 	r := chi.NewRouter()
-	_, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal("Cant connect to the db")
 	}
+	h := Handler{pool}
+	r.Get("/test", h.LocationHandler)
 	http.ListenAndServe("localhost:8000", r)
 }
