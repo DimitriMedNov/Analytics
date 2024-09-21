31ea35be8ded2486ff623018caea8354ebb0c50a
victor <vjbsh@hotmail.com>
2024-09-20 21:20:21 -0600

    created at

--- db/migrations/20240921023218_table.sql
+++ db/migrations/20240921023218_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS locations (
     id BIGSERIAL PRIMARY KEY,
     lat NUMERIC(10, 8),
     long NUMERIC(10, 8)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- migrate:down
