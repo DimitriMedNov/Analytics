cccdc6d7e5aa28ad942f9fbf3eb599160fdd5fb1
victor <vjbsh@hotmail.com>
2024-10-03 11:37:56 -0600

    alv

--- endpoints/forms.go
+++ endpoints/forms.go
@@ -43,3 +43,32 @@ func (h Handler) CreateQuestion(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write(response)
 }
+func (h Handler) CreateForm(w http.ResponseWriter, r *http.Request) {
+	var input QuestionInput
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		log.Println("Error unmarshalling JSON:", err)
+		return
+	}
+
+	qCreated, err := dbgen.New(h.Pool).CreateQuestion(context.Background(), dbgen.CreateQuestionParams{
+		Question:   input.Question,
+		AnswerType: input.AnswerType,
+		Options:    input.Options,
+	})
+	if err != nil {
+		log.Println("Error inserting to the DB:", err)
+		return
+	}
+
+	response, err := json.Marshal(qCreated)
+	if err != nil {
+		http.Error(w, "Error processing input", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(response)
+}
