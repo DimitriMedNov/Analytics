3c3e9dc27659c99fd74c2ce1a19e76f390341747
Jesus D'mitri Medina Novelo <00396910@anahuac.mx>
2024-10-04 15:59:41 -0600

    migrations embed

--- main.go
+++ main.go
@@ -3,30 +3,63 @@ package main
 import (
 	"analytics/endpoints"
 	"context"
+	"embed"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
+	"github.com/amacneil/dbmate/v2/pkg/dbmate"
+	_ "github.com/amacneil/dbmate/v2/pkg/driver/postgres"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+//go:embed db/migrations/*.sql
+var fs embed.FS
+
 func main() {
-	r := chi.NewRouter()
-	pool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatal("Cant connect to the db")
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL no está definida")
 	}
+
+	u, err := url.Parse(dbURL)
+	if err != nil {
+		log.Fatalf("Error al parsear DATABASE_URL: %v", err)
+	}
+
+	db := dbmate.New(u)
+	db.FS = fs
+
+	fmt.Println("Aplicando migraciones...")
+	err = db.CreateAndMigrate()
+	if err != nil {
+		log.Fatalf("Error aplicando migraciones: %v", err)
+	}
+
+	pool, err := pgxpool.New(context.Background(), dbURL)
+	if err != nil {
+		log.Fatalf("No se puede conectar a la base de datos: %v", err)
+	}
+	defer pool.Close()
+
+	r := chi.NewRouter()
 	h := endpoints.Handler{Pool: pool}
+
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+
+	// Definimos las rutas
 	r.Post("/location", h.LocationHandler)
 	r.Post("/question", h.CreateQuestion)
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) })
-	log.Printf("Listening on http://0.0.0.0:8080")
-	err = http.ListenAndServe("0.0.0.0:8080", r)
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusOK) })
+
+	log.Println("Escuchando en http://0.0.0.0:8000")
+	err = http.ListenAndServe("0.0.0.0:8000", r)
 	if err != nil {
-		log.Println(err)
+		log.Fatalf("Error al iniciar el servidor: %v", err)
 	}
 }
